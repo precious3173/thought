eb7e8b3ec31f6d549258c190d11a0f99cfebf743
Admin <preciouspeter3173@gmail.com>
2024-03-26 00:32:39 +0100

    sequence generator added

--- thought/src/main/kotlin/com/example/thought/model/Bank.kt
+++ thought/src/main/kotlin/com/example/thought/model/Bank.kt
@@ -1,6 +1,21 @@
 package com.example.thought.model
 
+import jakarta.persistence.*
+
+@Entity
+@Table
 data class Bank(
+        @Id
+        @SequenceGenerator(
+         name = "bank_sequence",
+         sequenceName = "bank_sequence",
+         allocationSize = 1
+        )
+        @GeneratedValue(
+                strategy = GenerationType.SEQUENCE,
+                generator = "banks"
+        )
+        val id: Long,
         val accountNumber: String,
         val name: String,
         val transactionFee: Int
