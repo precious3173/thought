2541286e669c85964c6b7c08f7229885175eea19
Admin <preciouspeter3173@gmail.com>
2024-03-28 22:15:57 +0100

    repository added

--- thought/src/main/kotlin/com/example/thought/datasource/BankDataSource.kt
+++ thought/src/main/kotlin/com/example/thought/datasource/BankDataSource.kt
@@ -2,8 +2,10 @@ package com.example.thought.datasource
 
 import com.example.thought.model.Bank
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-interface BankDataSource{
+@Repository
+interface BankDataSource: JpaRepository<Bank, Long>{
 
 fun getBank(): Collection<Bank>
 }
