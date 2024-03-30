c66c28ce4445f649c0e02a504b4abd242507a870
Admin <preciouspeter3173@gmail.com>
2024-03-30 23:28:34 +0100

    command line added

--- thought/src/main/kotlin/com/example/thought/Config/BankConfig.kt
+++ thought/src/main/kotlin/com/example/thought/Config/BankConfig.kt
@@ -1,4 +1,17 @@
 package com.example.thought.Config
 
+import org.springframework.boot.CommandLineRunner
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class BankConfig {
+
+    @Bean
+    fun commandLineRunner(): CommandLineRunner{
+        return CommandLineRunner { args ->
+
+
+        }
+    }
 }
